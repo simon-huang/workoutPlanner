094c15493419cf6c371a3ff17dfd69281e47ae34
Simon Huang <simon.t.huang@gmail.com>
2021-02-11 23:43:44 -0800

    Fix workout notation

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 angular.module('app', ['ngRoute', 'ui.sortable'])
-  .config(function($routeProvider) {
+  .config(function ($routeProvider) {
     $routeProvider
       .when('/templates', {
         templateUrl: 'templates.html',
@@ -13,21 +13,21 @@ angular.module('app', ['ngRoute', 'ui.sortable'])
         redirectTo: '/templates'
       });
   })
-  .factory('Template', function() {
+  .factory('Template', function () {
     var exer
-    var add = function() {
+    var add = function () {
     };
     return {
       add: add
     };
   })
-  .controller('newCtrl', function($scope, $http, $location) {
+  .controller('newCtrl', function ($scope, $http, $location) {
     $scope.data = {};
     $scope.data.name = "";
     $scope.data.exercises = [];
     $scope.selected = null;
     $scope.adding = false;
-    $scope.added = {name:null, amount:null};
+    $scope.added = { name: null, amount: null };
     $scope.editing = false;
     $scope.edited = {};
     $scope.sampling = false;
@@ -36,33 +36,39 @@ angular.module('app', ['ngRoute', 'ui.sortable'])
     $scope.samples = {
       list: ['Leg Day', 'Push Day', 'Pull Day'],
       list2: [
-        {name: 'Leg Day', exercises: [
-          {name: 'Deadlift', amount: '100x5x5'}, 
-          {name: 'Leg Curl', amount: '100x8x3'}, 
-          {name: 'Leg Press', amount: '100x5x5'}, 
-          {name: 'Calf Raise', amount: '100x8x3'}
-          ]},
-        {name: 'Push Day', exercises: [
-          {name: 'Bench Press', amount: '100x5x5'}, 
-          {name: 'Incline Bench Press', amount: '100x8x3'}, 
-          {name: 'Military Press', amount: '100x5x5'}, 
-          {name: 'Triceps Pushdown', amount: '100x8x3'}
-        ]},
-        {name: 'Pull Day', exercises: [
-          {name: 'Deadlift', amount: '100x5x5'}, 
-          {name: 'Lat Pulldown', amount: '100x5x5'}, 
-          {name: 'Dumbbell Rows', amount: '100x8x3'}, 
-          {name: 'Dumbbell Curls', amount: '100x8x3'}
-        ]}
+        {
+          name: 'Leg Day', exercises: [
+            { name: 'Squats', amount: '5x5' },
+            { name: 'Leg Curl', amount: '3x10' },
+            { name: 'Leg Press', amount: '3x10' },
+            { name: 'Calf Raise', amount: '3x10' }
+          ]
+        },
+        {
+          name: 'Push Day', exercises: [
+            { name: 'Bench Press', amount: '5x5' },
+            { name: 'Incline Bench Press', amount: '3x8' },
+            { name: 'Military Press', amount: '3x8' },
+            { name: 'Triceps Pushdown', amount: '3x10' }
+          ]
+        },
+        {
+          name: 'Pull Day', exercises: [
+            { name: 'Deadlift', amount: '5x5' },
+            { name: 'Lat Pulldown', amount: '3x10' },
+            { name: 'Dumbbell Rows', amount: '3x10' },
+            { name: 'Dumbbell Curls', amount: '3x10' }
+          ]
+        }
       ]
     };
 
     $scope.data = {
-      name:'New Workout',
+      name: 'New Workout',
       exercises: []
-    }; 
-    
-    $scope.select = function(exercise) {
+    };
+
+    $scope.select = function (exercise) {
       if ($scope.selected === exercise) {
         $scope.selected = null;
         $scope.edited = {};
@@ -73,38 +79,38 @@ angular.module('app', ['ngRoute', 'ui.sortable'])
       }
       console.log($scope.selected);
     }
-    var findIndex = function() {
+    var findIndex = function () {
       for (var i = 0; i < $scope.data.exercises.length; i++) {
         if ($scope.data.exercises[i].name === $scope.selected.name) {
           return i;
         }
       }
     };
-    $scope.add = function() {
+    $scope.add = function () {
       $scope.data.exercises.push($scope.data.added);
       $scope.data.added = {};
       console.log($scope.data.exercises);
     };
-    $scope.remove = function() {
+    $scope.remove = function () {
       var index = findIndex();
-      $scope.data.exercises.splice(index,1);
+      $scope.data.exercises.splice(index, 1);
       $scope.selected = null;
     };
-    $scope.toggleEdit = function() {
+    $scope.toggleEdit = function () {
       $scope.editing = !$scope.editing;
     };
-    $scope.edit = function() {
+    $scope.edit = function () {
       $scope.selected.name = $scope.edited.name;
       $scope.selected.amount = $scope.edited.amount;
       $scope.edited = {};
       $scope.toggleEdit();
     };
-    $scope.loadSample = function(workout) {
+    $scope.loadSample = function (workout) {
       console.log(workout);
       $scope.data = JSON.parse(JSON.stringify(workout));
       $scope.sampling = !$scope.sampling;
     };
-    $scope.save = function() {
+    $scope.save = function () {
       console.log($scope.data, typeof $scope.data);
       console.log(JSON.stringify($scope.data), typeof JSON.stringify($scope.data));
       $http({
@@ -113,48 +119,48 @@ angular.module('app', ['ngRoute', 'ui.sortable'])
         data: JSON.stringify($scope.data),
         header: {
           'Content-Type': 'application/json'
-          },
+        },
       })
-      .then(function() {
-        console.log('success');
-        $scope.data = {name: 'New Workout', exercises: []};
-      })
-      .catch(function(error) {
-        console.log('error: ', error);
-      });
+        .then(function () {
+          console.log('success');
+          $scope.data = { name: 'New Workout', exercises: [] };
+        })
+        .catch(function (error) {
+          console.log('error: ', error);
+        });
     }
   })
-  .controller('templatesCtrl', function($scope, $http) {
-    $scope.data = {list: []};
+  .controller('templatesCtrl', function ($scope, $http) {
+    $scope.data = { list: [] };
     $scope.selected = null;
-    $scope.selecting = function(which) {
+    $scope.selecting = function (which) {
       console.log('did');
       $scope.selected = which;
 
     };
-    $scope.retrieve = function() {
+    $scope.retrieve = function () {
       $http({
         method: 'GET',
         url: '/api/workouts'
       })
-      .then(function(res) {
-        // console.log(res.data[res.data.length-1].exercises, typeof res.data[res.data.length-1].exercises);
-        res.data.forEach(function(w) {
-          $scope.data.list.push({
-            name: w.name,
-            exercises: JSON.parse(w.exercises)
+        .then(function (res) {
+          // console.log(res.data[res.data.length-1].exercises, typeof res.data[res.data.length-1].exercises);
+          res.data.forEach(function (w) {
+            $scope.data.list.push({
+              name: w.name,
+              exercises: JSON.parse(w.exercises)
+            });
           });
+          // $scope.data.workouts = res.data;
+          console.log($scope.data.list);
+          // console.log(typeof $scope.data.list[2].workouts);
+          // console.log(JSON.parse($scope.data.list[2].workouts));
+        })
+        .catch(function (error) {
+          console.log('error: ', error);
         });
-        // $scope.data.workouts = res.data;
-        console.log($scope.data.list);
-        // console.log(typeof $scope.data.list[2].workouts);
-        // console.log(JSON.parse($scope.data.list[2].workouts));
-      })
-      .catch(function(error) {
-        console.log('error: ', error);
-      });
     }
 
     $scope.retrieve();
   });
-  
+
